57fa117b1f9ea657343fd34255ac25368fa003c1
jkewgns <wig24003978@heartofyorkshire.ac.uk>
2025-05-01 16:05:49 +0100

    Keypads 2/3

--- Retro VHS Test/Assets/Scripts/Keypad.cs
+++ Retro VHS Test/Assets/Scripts/Keypad.cs
@@ -7,57 +7,77 @@ using TMPro;
 public class Keypad : MonoBehaviour
 {
     public GameObject player;
-    public GameObject keypadObj;
+    public GameObject playerCam;
+    public GameObject keypadUI;
+    public GameObject scanner;
+    public GameObject levelLoader;
 
     public TMP_Text textObj;
+
     public string answer = "1234";
 
-    public AudioSource button;
-    public AudioSource correct;
-    public AudioSource wrong;
+    //public AudioSource button;
+    //public AudioSource correct;
+    //public AudioSource wrong;
 
-    void Start()
+    IEnumerator Start()
     {
-        keypadObj.SetActive(false);
+        keypadUI.SetActive(false);
+        
+        yield return new WaitForSeconds(5);
+        levelLoader.SetActive(false);
     }
 
     public void Number(int number)
     {
         textObj.text += number.ToString();
-        button.Play();
+        //button.Play();
     }
 
     public void Execute()
     {
         if (textObj.text == answer)
         {
-            correct.Play();
+            //correct.Play();
             textObj.text = "Right";
+            levelLoader.SetActive(true);
         }
         else
         {
-            wrong.Play();
+            //wrong.Play();
             textObj.text = "Wrong";
+            levelLoader.SetActive(false);
         }
     }
 
     public void Clear()
     {
         textObj.text = "";
-        button.Play();
+        //button.Play();
     }
 
     public void Exit()
     {
-        keypadObj.SetActive(false);
+        keypadUI.SetActive(false);
         player.GetComponent<PlayerMovement>().enabled = true;
+        playerCam.GetComponent<MouseLook>().enabled = true;
+        scanner.SetActive(true);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void Update()
     {
-        if (keypadObj.activeInHierarchy)
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            keypadUI.SetActive(true);
+        }
+
+        if (keypadUI.activeInHierarchy)
         {
             player.GetComponent<PlayerMovement>().enabled = false;
+            playerCam.GetComponent<MouseLook>().enabled = false;
+            scanner.SetActive(false);
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }        
--- Retro VHS Test/Assets/Scripts/OpenKeyPad.cs
+++ Retro VHS Test/Assets/Scripts/OpenKeyPad.cs
@@ -7,7 +7,12 @@ public class OpenKeyPad : MonoBehaviour
     public GameObject keypadObj;
     public GameObject keypadText;
 
+    public Transform player;
+
     public bool inReach;
+
+    public float activationDistance = 3f;
+    private bool isNearKeypad = false;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +29,7 @@ public class OpenKeyPad : MonoBehaviour
         }
     }
 
+/*
     void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Reach")
@@ -32,13 +38,35 @@ public class OpenKeyPad : MonoBehaviour
             keypadText.SetActive(false);
         }
     }
-
+*/
     // Update is called once per frame
     void Update()
     {
+/*
         if (Input.GetButtonDown("Interact")  && inReach)
         {
             keypadObj.SetActive(true);
+        }
+*/
+        float distance = Vector3.Distance(player.position, keypadObj.transform.position);
+        if (distance <= activationDistance)
+        {
+            if (!isNearKeypad)
+            {
+                isNearKeypad = true;
+                keypadText.SetActive(true);
+                keypadObj.GetComponent<Keypad>().enabled = true;
+            }
+        }
+        else
+        {
+            if (isNearKeypad)
+            {
+                isNearKeypad = false;
+                keypadText.SetActive(false);
+                keypadObj.GetComponent<Keypad>().enabled = false;
+                
+            }
         }
     }
 }
